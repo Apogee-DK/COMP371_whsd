34e18fdb50969564a97e4c1e5b50a5a54813fa1a
Dexter K <Apogee-DK@users.noreply.github.com>
2016-04-01 13:46:47 -0400

    Create Scene.h

---
+++ Source/Scene.h
@@ -0,0 +1,75 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "glm.hpp"
+#include "glfw3.h" 
+#include "Camera.h"
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Scene{
+private:
+	//Setting up the map dimensions
+	
+	Camera scene_camera;
+	glm::vec3 previous_camera_position;
+
+	int length_map;
+	int width_map;		
+	int numOfTrees;
+	int numOfHouses;
+	int numOfHills;
+	float size_cube;
+
+	//Boundaries of the map
+	float min_x = -0.1f;
+	float min_y = -0.1f;
+	float max_z = 0.1f;
+	
+	float max_x;
+	float max_y;
+	float min_z;
+
+
+public:
+
+	Scene();
+
+	//Set the size of map and the size of the cubes
+	Scene(Camera, int, int, float);
+
+	void update();
+
+	void setSceneWidth(int);
+
+	void setSceneLength(int);
+
+	void setSceneCubeSize(float);
+
+	//Set objects functions
+	void setNumberOfTrees(int);
+
+	void setNumberOfHouses(int);
+
+	void setNumberOfHills(int);
+
+	void setSceneCamera(Camera);
+
+	void setPreviousCameraPosition(glm::vec3);
+	
+	glm::vec3 getMinBoundaryMap();
+
+	glm::vec3 getMaxBoundaryMap();
+
+	int getSceneWidth();
+
+	int getSceneLength();
+
+	Camera& getSceneCamera();
+
+	glm::vec3 Scene::getPreviousSceneCameraPosition();
+
+
+};
+#endif
